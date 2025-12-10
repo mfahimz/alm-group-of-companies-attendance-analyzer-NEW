83c4232926385c94297b7cd8d9707bf52c1fba6c
binfah222 <binfah222@gmail.com>
2025-12-10 01:55:28 +0000

    File changes

---
+++ src/components/project-tabs/ReportDetailView.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { useQuery, useMutation, useQueryClient } from '@tantml:function_calls>
+<invoke name="$FUNCTION_NAME">
+<parameter name="$PARAMETER_NAME">$PARAMETER_VALUE
