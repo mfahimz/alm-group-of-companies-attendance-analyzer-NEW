95d0695a3008b34b8e229db69431915685f3ba4b
binfah222 <binfah222@gmail.com>
2025-12-12 14:50:56 +0000

    File changes

--- src/components/project-tabs/OverviewTab.jsx
+++ src/components/project-tabs/OverviewTab.jsx
@@ -14,6 +14,7 @@ import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '../../utils';
 import AnomalyDetectionCard from './AnomalyDetectionCard';
 import EmployeeSelectionDialog from '../projects/EmployeeSelectionDialog';
+import CloseProjectDialog from './CloseProjectDialog';
 
 export default function OverviewTab({ project }) {
     const [showCloseDialog, setShowCloseDialog] = useState(false);
