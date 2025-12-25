4de8f7592b527f3361ef42158f6d6bb027c60c73
binfah222 <binfah222@gmail.com>
2025-12-25 02:06:16 +0000

    File changes

--- src/pages/AuditTrail.jsx
+++ src/pages/AuditTrail.jsx
@@ -327,7 +327,8 @@ export default function AuditTrail() {
                                                         year: 'numeric',
                                                         hour: '2-digit',
                                                         minute: '2-digit',
-                                                        second: '2-digit'
+                                                        second: '2-digit',
+                                                        timeZone: 'Asia/Dubai'
                                                     })}
                                                 </td>
                                                 <td className="px-4 py-3">
