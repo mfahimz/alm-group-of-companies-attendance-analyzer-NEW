c34c49d44eb98085f4c248e07163bd5773f06314
binfah222 <binfah222@gmail.com>
2026-01-27 04:37:26 +0000

    File changes

--- src/components/project-tabs/ReportTab.jsx
+++ src/components/project-tabs/ReportTab.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery, useMutation, useQueryClient } from '@anstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
