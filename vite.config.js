4ae6bd6ce170e134d89e786bc7b62677f07697e3
mfahimz <binfah222@gmail.com>
2026-03-16 21:51:50 +0400

    feat: add cache busting and service worker unregister logic to vite config

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,35 @@ export default defineConfig({
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true'
     }),
     react(),
-  ]
+    {
+      name: 'cache-control',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</head>',
+          `  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Expires" content="0" />
+  <script>
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for(let registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
+  </script>
+</head>`
+        );
+      }
+    }
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 });
